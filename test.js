a4d1bf72045c23d47af6a892c459ca254fa1e50f
Julia Martin <juliadmartin720@gmail.com>
2022-03-09 14:01:03 -0800

    Refactor SDK and write two tests using userId

--- test.js
+++ test.js
@@ -1,15 +1,26 @@
 Config = require('./Config');
 
-const config = new Config('12345', "http://localhost:5000", 1000).connect();
-const sdkClient = config.client;
-// sdkClient.addContext({ userId: "abcde" });
+let sdkClient;
+// Testing "Test flag 2", which is ON and has percentage 50% (based on user hash).
 
-const logStatus = () => {
-  if (sdkClient.evaluateFlag("Test Flag 1")) {
+// Testing userId "5555" => Flag is On
+(async () => {
+  sdkClient = await new Config('12345', "http://localhost:5000").connect();
+  sdkClient.addContext({ userId: "5555" });
+  if (sdkClient.evaluateFlag("Test flag 2")) {
     console.log("Flag is on");
   } else {
     console.log("Flag is off");
   }
-}
+})();
 
-setInterval(logStatus, 1000);
+// Testing userId "abcde" => Flag is Off
+(async () => {
+  sdkClient = await new Config('12345', "http://localhost:5000").connect();
+  sdkClient.addContext({ userId: "abcde" });
+  if (sdkClient.evaluateFlag("Test flag 2")) {
+    console.log("Flag is on");
+  } else {
+    console.log("Flag is off");
+  }
+})();
